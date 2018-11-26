caa648205a8d95925e01c26829390434d8b74698
Morgan Peat <morgan.peat@natwestmarkets.com>
2018-11-26 11:20:06 +0000

    Got it working, deploys basic site with iis

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# Ensure IIS is installed
+dsc_resource "install iis" do
+    resource :windowsfeature
+    property :name, 'Web-Server'
+    property :ensure, 'Present'
+end
